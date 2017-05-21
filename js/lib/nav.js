f09f6fa45984a829e96f839600f7be26bbfb07fe
chrios <c.frew19@gmail.com>
2017-05-21 16:32:31 +0930

    Major commit

---
+++ js/lib/nav.js
@@ -0,0 +1,23 @@
+/* adapted from 
+https://gist.github.com/taniarascia/56893ff29f64038dca91
+https://www.taniarascia.com/responsive-dropdown-navigation-bar/
+*/
+
+(function($) {
+  $(function() {
+    $('nav ul li > a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+  });
+  document.querySelector('#nav-toggle').addEventListener('click', function() {
+    this.classList.toggle('active');
+  });
+  $('#nav-toggle').click(function() {
+    $('nav ul').toggle();
+  });
+})(jQuery);
--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 /**
-* Scripts for {Project name and url}
+* Script to show.hide tables
 *
-* author: {name}
-* email: {email}
-* website: {portfolio}
+* author: christopher
+* email: c.frew19@gmail.com
 */
+
+document.getElementById("table-1-toggle").addEventListener("click", function() {
+	if (document.getElementById("table-1").className == "active") {
+		document.getElementById("table-1").className = "notactive";
+	} else {
+		document.getElementById("table-1").className = "active";
+	}
+});
+
+document.getElementById("table-2-toggle").addEventListener("click", function() {
+	if (document.getElementById("table-2").className == "active") {
+		document.getElementById("table-2").className = "notactive";
+	} else {
+		document.getElementById("table-2").className = "active";
+	}
+})
