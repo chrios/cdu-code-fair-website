b53970a72c379dde67c6468ccb948e529945f14d
chrios <c.frew19@gmail.com>
2017-05-25 22:39:14 +0930

    should be final commit

--- js/lib/nav.js
+++ js/lib/nav.js
@@ -6,11 +6,18 @@ https://www.taniarascia.com/responsive-dropdown-navigation-bar/
 (function($) {
 
   document.querySelector('#nav-toggle').addEventListener('click', function() {
-    this.classList.toggle('active');        /*sets up the class to be toggled on and of the nav <ul>*/
+    this.classList.toggle('active');        /*toggles the active class on the hamburger click button*/
   });
 
   $('#nav-toggle').click(function() {   /*#nav-toggle is the <a> wrapping the <span>'s of the hamburger menu*/
     $('nav ul').toggle();               /*toggle() is jquery function which toggles classes on and off*/
   });
+
+  $('.nav-list-item').click(function() {
+  	$('nav ul').toggle();
+  	document.querySelector('#nav-toggle').classList.toggle('active');  
+  });
+
+
 
 })(jQuery);
